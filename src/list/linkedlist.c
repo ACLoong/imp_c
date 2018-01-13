49db1d9c670f1fa6a74587e5b168a6df29faf11b
WangQing <kingchin1218@126.com>
2018-01-13 12:10:25 +0800

    add samples

--- src/list/linkedlist.c
+++ src/list/linkedlist.c
@@ -4,3 +4,135 @@
 
 #include "linkedlist.h"
 
+#include <stdlib.h>
+
+struct llist_t *llist_create()
+{
+    struct llist_t *list = malloc(sizeof(struct llist_t));
+    if (NULL != list) {
+        list->head = NULL;
+        list->tail = NULL;
+        list->len = 0;
+    }
+
+    return list;
+}
+
+int llist_insert(struct llist_t *list, size_t pos, void *data)
+{
+    //TODO
+}
+
+void llist_remove(struct llist_t *list, size_t pos)
+{
+    //TODO
+}
+
+int llist_lpush(struct llist_t *list, void *data)
+{
+    //TODO
+}
+
+int llist_rpush(struct llist_t *list, void *data)
+{
+    //TODO
+}
+
+void *llist_lpop(struct llist_t *list)
+{
+    //TODO
+}
+
+void *llist_rpop(struct llist_t *list)
+{
+    //TODO
+}
+
+void *llist_get(struct llist_t *list, size_t pos)
+{
+    //TODO
+}
+
+ssize_t llist_find(struct llist_t *list, void *data)
+{
+    //TODO
+}
+
+int llist_modify(struct llist_t *list, size_t pos, void *data)
+{
+    //TODO
+}
+
+void llist_clear(struct llist_t *list)
+{
+    //TODO
+}
+
+void llist_destroy(struct llist_t *list)
+{
+    if (NULL != list) {
+        llist_clear(list);
+        free(list);
+        list = NULL;
+    }
+}
+
+struct llist_iter_t *llist_iter_create(struct llist_t *list)
+{
+    //TODO
+}
+
+void llist_iter_destroy(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_attach(struct llist_iter_t *iter, struct llist_t *list)
+{
+    //TODO
+}
+
+void llist_iter_detach(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_next(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_prev(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_head(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_tail(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_seek_head(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+void llist_iter_seek_tail(struct llist_iter_t *iter)
+{
+    //TODO
+}
+
+struct llist_node_t *llist_node_create()
+{
+    //TODO
+}
+
+void llist_node_destroy(struct llist_node_t *node)
+{
+    //TODO
+}
--- src/list/linkedlist.h
+++ src/list/linkedlist.h
@@ -11,22 +11,22 @@
 extern "c" {
 #endif
 
-typedef struct {
+struct llist_node_t{
     struct llist_node_t *prev;
     struct llist_node_t *next;
     void *data;
-} llist_node_t;
+} ;
 
-typedef struct {
+struct llist_t{
     struct llist_node_t *head;
     struct llist_node_t *tail;
     size_t len;
-} llist_t;
+} ;
 
-typedef struct {
+struct llist_iter_t{
     struct llist_t *list;
     struct llist_node_t *curr_node;
-}llist_iter_t;
+} ;
 
 struct llist_t *llist_create();
 int llist_insert(struct llist_t *list, size_t pos, void *data);
@@ -35,25 +35,25 @@ int llist_lpush(struct llist_t *list, void *data);
 int llist_rpush(struct llist_t *list, void *data);
 void *llist_lpop(struct llist_t *list);
 void *llist_rpop(struct llist_t *list);
-void* llist_get(llist_t *list, size_t pos);
+void *llist_get(struct llist_t *list, size_t pos);
 ssize_t llist_find(struct llist_t *list, void *data);
 int llist_modify(struct llist_t *list, size_t pos, void *data);
 void llist_clear(struct llist_t *list);
 void llist_destroy(struct llist_t *list);
 
-llist_iter_t *llist_iter_create(struct llist_t *list);
+struct llist_iter_t *llist_iter_create(struct llist_t *list);
 void llist_iter_destroy(struct llist_iter_t *iter);
 void llist_iter_attach(struct llist_iter_t *iter, struct llist_t *list);
 void llist_iter_detach(struct llist_iter_t *iter);
-void llist_iter_seek_head(struct llist_iter_t *iter);
-void llist_iter_seek_tail(struct llist_iter_t *iter);
 void llist_iter_next(struct llist_iter_t *iter);
 void llist_iter_prev(struct llist_iter_t *iter);
 void llist_iter_head(struct llist_iter_t *iter);
 void llist_iter_tail(struct llist_iter_t *iter);
+void llist_iter_seek_head(struct llist_iter_t *iter);
+void llist_iter_seek_tail(struct llist_iter_t *iter);
 
-llist_node_t *llist_node_create();
-void llist_node_destroy(llist_node_t *node);
+struct llist_node_t *llist_node_create();
+void llist_node_destroy(struct llist_node_t *node);
 
 #if defined(__cplusplus)
 }
